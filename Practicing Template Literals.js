92c3bf31bab9072e0f6b3131b6fb0ab26639f961
Huzaifa Muhammad Siddique <88268541+HuzaifaMuhammadSiddique@users.noreply.github.com>
2023-04-18 06:15:19 +0300

    Create Practicing Template Literals.js

---
+++ Practicing Template Literals.js
@@ -0,0 +1,11 @@
+let a = 2;
+let b = 5;
+let c = 11;
+
+let sumAB = a+b;
+let sumBC = b+c;
+let sumAC = a+c;
+
+console.log(`The sum of ${a} + ${b} = ${sumAB}`);
+console.log(`The sum of ${b} + ${c} = ${sumBC}`);
+console.log(`The sum of ${a} + ${c} = ${sumAC}`);
