3b60b2252273d7f87de3e0809898b172656f7a68
Huzaifa Muhammad Siddique <88268541+HuzaifaMuhammadSiddique@users.noreply.github.com>
2023-04-21 22:48:43 +0300

    Create FCC problem 12.js

---
+++ FCC problem 12.js
@@ -0,0 +1,29 @@
+// PROBLEM
+/*
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted.
+The returned value should be a number.
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and
+19 is less than 20 (index 2) and greater than 5 (index 1).
+*/
+
+// SOLUTION
+function getIndexToIns(arr, num) {
+  arr.sort((a,b) => a - b)
+  if(arr.length === 0){
+    return 0
+  }
+  if(num <= arr[0]){
+    return 0;
+  }
+
+  for(let i=0; i<arr.length; i++){
+      if((arr[i] < num) && (arr[i+1] >= num)){
+          return i+1
+      }
+  }
+  
+  return arr.length;
+}
+
+getIndexToIns([40, 60], 50);
