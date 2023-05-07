e45cef8a7b0898d4460404c3b82e05a5dd189492
Huzaifa Muhammad Siddique <88268541+HuzaifaMuhammadSiddique@users.noreply.github.com>
2023-05-07 17:28:07 +0300

    Create Repeats Only in an Array.js

---
+++ Repeats Only in an Array.js
@@ -0,0 +1,28 @@
+// PROBLEM
+/*
+Write a function that takes an array of integers as input and returns a new array that contains only the elements that appear more than once in the original array.
+*/
+
+// QUESTION
+const moreThanOne = (array) => {
+  let newArray = [];
+  for(let i=0;i<array.length;i++){
+    if(array.indexOf(array[i]) !== array.lastIndexOf(array[i])){
+      newArray.push(array[i]);
+    }
+  }
+  newArray = new Set(newArray);
+  newArray = Array.from(newArray);
+  return newArray;
+}
+
+// TESTS
+let a = [1,2,3,3,4,5,5,6,6,6,7,8,9,10,10];
+let b = [-3,-1,5,9,15,20];
+let c = [1,1,1,1,1,1];
+let d = [1,1,2,2,3,3,4,4];
+
+console.log(moreThanOne(a));
+console.log(moreThanOne(b));
+console.log(moreThanOne(c));
+console.log(moreThanOne(d));
